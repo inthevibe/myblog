c2047097fb557291726fe90031e81a8ce4776695
Anderson Ribeiro <andersonsribeiro@proton.me>
2025-09-01 16:15:09 -0300

    Footer socials

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/output.css": "style.css", });
   eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts", });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets", });
   eleventyConfig.addPassthroughCopy({ "src/favicon.png": "favicon.png" });
+  eleventyConfig.ignores.add("README.md");
 }
